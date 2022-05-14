65115a611b49c692a9434cc2d75e0e5ff34f145f
Gilson <gilpacsebastiao@hotmail.com>
2022-05-14 17:23:59 +0100

    Mexendo no Css do site

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,28 +1,55 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+$(document).ready(function(){
+  $(window).scroll(function(){
+      if(this.scrollY > 20){
+          $('.navbar').addClass("sticky");
+      }else{
+          $('.navbar').removeClass("sticky");
+      }
+      if(this.scrollY > 500){
+          $('.scroll-up-btn').addClass("show");
+      }else{
+          $('.scroll-up-btn').removeClass("show");
+      }
+  });
+   $('.scroll-up-btn').click(function(){
+       $('html').animate({scrollTop: 0});
+   });
 
-// 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+   var typed = new Typed(".typing", {
+       strings:["Desenvolvedor web", "Freelancer", "Desenhista", "Técnico de Hardware"],
+       typeSpeed:100,
+       backSpeed:60,
+       loop:true
+   });
+   var typed = new Typed(".typing-2", {
+      strings:["Desenvolvedor web", "Freelancer", "Desenhista", "Técnico de Hardware"],
+      typeSpeed:100,
+      backSpeed:60,
+      loop:true
+  });
 
-//
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+  $('.menu-btn').click(function(){
+      $('.navbar .menu').toggleClass("active");
+      $('.menu-btn i').toggleClass("active");
+  });
+  $('.carousel').owlCarousel({
+      margin:20,
+      loop:true,
+      autoplayTimeOut:2000,
+      autoplayHoverPauser:true,
+      responsive:{
+          0:{
+              items:1,
+              nav:false
+          },
+          600:{
+              items:2,
+              nav:false
+          },
+          1000:{
+              items:3,
+              nav:false
+          }
+      }
+  });
+});
